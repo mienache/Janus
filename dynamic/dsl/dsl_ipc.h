a289f84cf76df0be8cd53bc884d6d44a833743c7
Mihai Enache <mihai.enache96@gmail.com>
2022-04-13 16:11:01 +0100

    Add class for CometQueue

--- dynamic/dsl/dsl_ipc.h
+++ dynamic/dsl/dsl_ipc.h
@@ -1,13 +1,18 @@
 #ifndef __DSL_IPC__
 #define __DSL_IPC__
 
+#include <iostream>
 #include <map>
 
+#include <sys/mman.h>
+#include <unistd.h>
 #include <cstdint>
 
 #include "janus_api.h"
 
 struct BasicQueue;
+
+struct CometQueue;
 
 /*--- IPC Declarations Start ---*/
 
@@ -24,6 +29,57 @@ struct BasicQueue {
         begin = v;
         end = v;
     }
+};
+
+struct CometQueue {
+    int *z1;
+    int *z2;
+
+    CometQueue(size_t num_items_per_zone)
+    {
+        std::cout<< "Num items per zone: " << num_items_per_zone << std::endl;
+        std::cout<< "Size of a queue item is: " << sizeof(*z1) << std::endl;
+
+        const int prot = PROT_READ | PROT_WRITE;
+        const int flags = MAP_PRIVATE | MAP_ANONYMOUS;
+        const int fd = -1;
+        const size_t offset = 0;
+
+        const int page_size = getpagesize();
+        size_t num_items_per_page = page_size / sizeof(*z1);
+
+        std::cout<< "Size of a page = " << page_size << std::endl;
+        std::cout<< "Num items per page: " << num_items_per_page << std::endl;
+
+        const size_t pages_per_zone = (num_items_per_zone * sizeof(*z1)) / page_size + (((num_items_per_zone * sizeof(*z1)) % page_size) ? 1 : 0);
+        const size_t zone_size = pages_per_zone * page_size;
+        num_items_per_zone = zone_size / sizeof(*z1);
+
+        std::cout<< "Readjusted num items per zone: " << num_items_per_zone << std::endl;
+
+        std::cout<< "Allocating " << pages_per_zone << " pages per zone" << std::endl;
+
+        const int total_pages = 2 * (pages_per_zone + 1);
+        const size_t total_size = total_pages * page_size;
+
+        z1 = (int*) mmap(0, total_size, prot, flags, fd, offset);
+        std::cout<< "Allocated Z1 at " << (void*) z1 << std::endl;
+
+        int *r1 = z1 + num_items_per_zone;
+
+        std::cout<< "Trying to deallocate at " << (void*) r1 << std::endl;
+        std::cout<< "munmap successful? : " << munmap(r1, page_size) << std::endl;
+
+        int *r2 = z1 + 2 * num_items_per_zone + num_items_per_page;
+        std::cout<< "Trying to deallocate at " << (void*) r2 << std::endl;
+        std::cout<< "munmap successful? : " << munmap(r2, page_size) << std::endl;
+
+        z2 = z1 + num_items_per_zone + num_items_per_page;
+
+        std::cout<< "Z1 at " << (void*) z1 << std::endl;
+        std::cout<< "Z2 at " << (void*) z2 << std::endl;
+    }
+
 };
 
 extern BasicQueue *IPC_QUEUE;
