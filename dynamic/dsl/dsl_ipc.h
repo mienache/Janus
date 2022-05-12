69fd45961346fab8ad7c65708b2f63ffb0bd5b9c
Mihai Enache <mihai.enache96@gmail.com>
2022-05-12 16:53:15 +0100

    Remove unused functions from dsl_ipc

--- dynamic/dsl/dsl_ipc.h
+++ dynamic/dsl/dsl_ipc.h
@@ -113,10 +113,4 @@ extern CometQueue *IPC_QUEUE_2;
 BasicQueue* initialise_queue();
 CometQueue* initialise_comet_queue();
 
-void append_value(BasicQueue *queue, int val);
-int consume_value(BasicQueue *queue);
-void add_instrumentation_code_for_queue_communication(JANUS_CONTEXT, void *func, BasicQueue *queue, opnd_t dest);
-void enqueue(BasicQueue *queue, uint64_t register_value);
-void dequeue(BasicQueue *queue, uint64_t register_value);
-
 #endif
